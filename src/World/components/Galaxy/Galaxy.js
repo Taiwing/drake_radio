38527abf6c421cbad2e01ae35778ecc48f1eb7d0
yoann foreau <yoann.foreau@ito.am>
2021-12-27 13:47:37 +0100

    fix: reduce galactic center size

--- src/World/components/Galaxy/Galaxy.js
+++ src/World/components/Galaxy/Galaxy.js
@@ -2,11 +2,25 @@ import { Group, MathUtils } from '../../vendor/three.js'
 import { createSphere } from './sphere.js'
 import { createParticles } from './particles.js'
 
+/*
+** 1 unit = 20_000 light years
+** center diameter: 2 units - 40_000 light years
+** milky way diameter: 10 units - 200_000 light years
+** milky way height: 0.5 units - 10_000 light years
+*/
+
 export class Galaxy extends Group {
   constructor() {
     super()
-    this._center = createSphere()
-    this._stars = createParticles()
+    this._centerRadius = 1
+    this._galacticRadius = 5
+    this._galacticHeight = 0.5
+    this._center = createSphere({ radius: this._centerRadius })
+    this._stars = createParticles({
+      centerRadius: this._centerRadius,
+      radius: this._galacticRadius,
+      height: this._galacticHeight,
+    })
     this.add(this._center, this._stars)
     this._rotationPerSec = MathUtils.degToRad(10)
   }
--- src/World/components/Galaxy/particles.js
+++ src/World/components/Galaxy/particles.js
@@ -14,8 +14,8 @@ export const createParticles = (opt = {}) => {
     particleCount = 20000,
     particleSize = 0.01,
     height = 0.5,
-    width = 7,
-    centerWidth = 2,
+    radius = 5,
+    centerRadius = 1,
   } = opt
 
   const geometry = new BufferGeometry()
@@ -23,14 +23,14 @@ export const createParticles = (opt = {}) => {
     color: 'white',
     size: particleSize,
   })
-  const maxX = width / 2, maxY = height / 2
+  const maxX = radius, maxY = height / 2
   const vertices = []
   for (let i = 0; i < particleCount; i++) {
     const x = (maxX * Math.random()) * (Math.random() < 0.5 ? -1 : 1)
     const y = (maxY * Math.random()) * (Math.random() < 0.5 ? -1 : 1)
     const z = (maxX * Math.random()) * (Math.random() < 0.5 ? -1 : 1)
-    if (!isInsideSphere({ r: centerWidth / 2, x, y, z })
-      && isInsideSphere({ r: width / 2, x, y, z }))
+    if (!isInsideSphere({ r: centerRadius, x, y, z })
+      && isInsideSphere({ r: radius, x, y, z }))
       vertices.push(x, y, z)
   }
   geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3))
