042c5427cf56542e90b3c6323f72a7341b4b3f81
Yoann FOREAU <yoyo.fofo47@gmail.com>
2024-04-14 10:25:17 +0200

    fix: generalize run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -dit --name drake-radio -p 8080:80 -v ~/0me_projects/drake_radio:/usr/local/apache2/htdocs/ httpd:2.4
+sudo docker run -dit --name drake-radio -p 8080:80 -v ./:/usr/local/apache2/htdocs/ httpd:2.4
