25191d02c2df7e5fac80b2c9e2b4567f15ca37e0
Yoann FOREAU <yoyo.fofo47@gmail.com>
2024-07-16 12:00:59 +0200

    feat: speedup and speeddown buttons

--- www/src/controls.js
+++ www/src/controls.js
@@ -1,10 +1,18 @@
+import { simulation } from './drake.js'
+
 const ControlPanelTemplate = document.createElement('template')
 ControlPanelTemplate.innerHTML = `
 	<link href="./main.css" rel="stylesheet" type="text/css" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" />
 
+  <button title="" id="backward-button" class="main-button">
+    <i class="fa-solid fa-backward fa-2x" id="backward-icon"></i>
+  </button>
   <button title="" id="play-pause-button" class="main-button" style="width: 3em">
     <i class="fa-solid fa-play fa-2x" id="play-pause-icon"></i>
+  </button>
+  <button title="" id="forward-button" class="main-button">
+    <i class="fa-solid fa-forward fa-2x" id="forward-icon"></i>
   </button>
 `
 
@@ -29,15 +37,13 @@ class ControlPanel extends HTMLElement {
 
 customElements.define('control-panel', ControlPanel)
 
+const SPEED_FACTOR = 25 / 100
+
 export class Controls {
   constructor({ world }) {
     this._controlPanel = document.querySelector('control-panel')
     this._world = world
     this.loop = false
-
-    const playPauseButton = this._controlPanel.shadowRoot
-      .querySelector('#play-pause-button')
-    playPauseButton.addEventListener('click', () => this.playPauseToggle())
 
     window.addEventListener('keydown', (key) => {
       switch (key.code) {
@@ -49,6 +55,24 @@ export class Controls {
       }
       key.preventDefault()
     })
+
+    const backwardButton = this._controlPanel.shadowRoot
+      .querySelector('#backward-button')
+    backwardButton.addEventListener('click', () => this.speedDown())
+
+    const playPauseButton = this._controlPanel.shadowRoot
+      .querySelector('#play-pause-button')
+    playPauseButton.addEventListener('click', () => this.playPauseToggle())
+
+    const forwardButton = this._controlPanel.shadowRoot
+      .querySelector('#forward-button')
+    forwardButton.addEventListener('click', () => this.speedUp())
+  }
+
+  speedDown() {
+    const { current, min } = simulation['speed']
+    const newCurrent = Math.floor(current - current * SPEED_FACTOR)
+    simulation['speed'].current = newCurrent < min ? min : newCurrent
   }
 
   playPauseToggle() {
@@ -60,5 +84,11 @@ export class Controls {
     this.loop = !this.loop
 
     this._controlPanel.updatePlayPauseButton(this.loop)
+  }
+
+  speedUp() {
+    const { current, max } = simulation['speed']
+    const newCurrent = Math.ceil(current + current * SPEED_FACTOR)
+    simulation['speed'].current = newCurrent > max ? max : newCurrent
   }
 }
