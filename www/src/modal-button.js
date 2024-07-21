65eff741c359d3aa483ded0dfb286623ebbf0b0e
Yoann FOREAU <yoyo.fofo47@gmail.com>
2024-07-21 13:29:12 +0200

    feat: add auto-closing function to modal button
    Use the data-time attribute for this.

--- www/src/modal-button.js
+++ www/src/modal-button.js
@@ -2,7 +2,7 @@ const ModalButtonTemplate = document.createElement('template')
 ModalButtonTemplate.innerHTML = `
   <slot name="button" id="button" style="cursor: pointer"></slot>
   <dialog is="custom-dialog">
-    <slot name="modal"></slot>
+    <slot name="modal" id="modal"></slot>
   </dialog>
 `
 
@@ -14,7 +14,17 @@ class ModalButton extends HTMLElement {
 
     const dialog = this.shadowRoot.querySelector('dialog')
     const button = this.shadowRoot.querySelector('#button')
-    button.addEventListener('click', () => dialog.showModal())
+    button.addEventListener('click', () => {
+      dialog.showModal()
+      const time = this.getAttribute('data-time')
+      if (time) {
+        this.closeTimeout = setTimeout(() => dialog.close(), time)
+      }
+    })
+    dialog.addEventListener('close', () => {
+      clearTimeout(this.closeTimeout)
+      this.closeTimeout = undefined
+    })
   }
 }
 
