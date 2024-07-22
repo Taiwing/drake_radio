8ff027698fce4cbd796af1f00eeb5305c6cb1e70
Yoann FOREAU <yoyo.fofo47@gmail.com>
2024-07-22 02:10:26 +0200

    chore: remove semi-colons

--- www/src/utils.js
+++ www/src/utils.js
@@ -1,10 +1,10 @@
 export const formatNumber = (value, maxLength, fractionDigits = 2) => {
-  const valueStr = value.toString();
-  const effectiveLength = valueStr.replace('.', '').length;
+  const valueStr = value.toString()
+  const effectiveLength = valueStr.replace('.', '').length
 
   if (effectiveLength > maxLength) {
-    return value.toExponential(fractionDigits);
+    return value.toExponential(fractionDigits)
   } else {
-    return valueStr;
+    return valueStr
   }
 }
